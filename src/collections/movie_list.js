dd2f8a28d12f742b1712370058b070433a4331ac
kee nam <nam.keeyeon@gmail.com>
2017-12-18 13:56:34 -0800

    Added URL to movie.js and movie_list.js

--- src/collections/movie_list.js
+++ src/collections/movie_list.js
@@ -3,6 +3,10 @@ import Movie from 'models/movie';
 
 const MovieList = Backbone.Collection.extend({
   model: Movie,
+  urlRoot: `http://localhost:3000/movies`,
+  parse: function(response) {
+    return response;
+  },
 });
 
 export default MovieList;
--- src/models/movie.js
+++ src/models/movie.js
@@ -7,6 +7,11 @@ const Movie = Backbone.Model.extend({
   },
   validate(attributes) {
   },
+  urlRoot: `http://localhost:3000/movies/`,
+  parse: function(response) {
+    return response;
+  },
+  comparator: 'title',
 });
 
 export default Movie;
