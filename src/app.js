df3491c616b3fd8199fe93a335280d329a4675c0
Andrea Chen <andreachen.ac@gmail.com>
2017-12-18 15:43:37 -0800

    Commiting to switch to new branch

--- src/app.js
+++ src/app.js
@@ -11,14 +11,12 @@ import LibMovie from './models/lib_movie';
 import LibMovieList from './collections/lib_movie_list';
 import LibMovieView from './views/lib_movie_view';
 import LibMovieListView from './views/lib_movie_list_view';
+import SearchMovie from './models/search_movie';
+import SearchMovieList from './collections/search_movie_list';
 
 // template varibles
 
 let libraryMovieTemplate;
-
-
-
-
 
 // ready to go
 $(document).ready(function() {
--- src/collections/search_movie_list.js
+++ src/collections/search_movie_list.js
@@ -3,7 +3,7 @@ import SearchMovie from '../models/search_movie';
 
 const SearchMovieList = Backbone.Collection.extend({
   model: SearchMovie,
-  urlRoot: `http://localhost:3000/movies/search?query=${this.get('search_term')}`,
+  // urlRoot: `http://localhost:3000/movies/search?query=${this.get('search_term')}`,
 });
 
 export default SearchMovieList;
