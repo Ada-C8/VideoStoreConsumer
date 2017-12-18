bc51e8a7828cf6a7611a3b23968ef1d3b901ae99
Lauren <Laurenelee88@gmail.com>
2017-12-18 12:20:03 -0800

    movie view

---
+++ src/views/movie_view.js
@@ -0,0 +1,14 @@
+import Backbone from 'backbone';
+import Movie from '../models/movie';
+
+const MovieView = Backbone.View.extend({
+
+  initialize(params) {
+  },
+
+});
+
+
+
+
+export default MovieView;
