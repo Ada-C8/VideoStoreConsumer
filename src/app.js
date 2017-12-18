fa62cb506c0917f4bc91d24c9f9ae678db37fd5a
Amy Lee <amy.yj.lee@gmail.com>
2017-12-18 13:38:07 -0800

    imported models, collections and views for Movie and Movie List to app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ import './css/styles.css';
 // Import jQuery & Underscore
 import $ from 'jquery';
 import _ from 'underscore';
+import Movie from 'models/movie';
+import MovieList from 'collects/movie_list';
+import MovieView from 'views/movie';
+import MovieListView from 'view/movie_list';
 
 // ready to go
 $(document).ready(function() {
