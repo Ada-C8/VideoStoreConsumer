9dffff7eb59e98bd5f8f1aa0bc66c98cc82503e9
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-12-18 13:56:28 -0800

    MovieList collection takes optional query parameter

--- src/collections/movie_list.js
+++ src/collections/movie_list.js
@@ -3,7 +3,17 @@ import Movie from '../models/movie';
 
 const MovieList = Backbone.Collection.extend({
   model: Movie,
-  url: 'http://localhost:3000/movies',
+  url: function() {
+    return 'http://localhost:3000/movies/' + this.query;
+  },
+
+  initialize(params) {
+    if (params){
+      this.query = params.query ? `/search?query=${params.query}` : '';
+    } else {
+      this.query = '';
+    }
+  },
 });
 
 export default MovieList;
