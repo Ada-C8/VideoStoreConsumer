d0e03b788a4f6352cf6a37d5aadfe583c84a5749
Retropiaf (Christiane Sawadogo) <sawadogochristiane@gmail.com>
2017-12-18 10:57:21 -0800

    Create Video model, Video view Video collection, video collection view

---
+++ src/views/video_view.js
@@ -0,0 +1,16 @@
+import Backbone from 'backbone';
+
+const VideoView = Backbone.View.extend({
+  initialize(parameters) {
+
+  },
+  render() {
+
+    return this;
+  },
+  events: {
+    // click add to library
+  },
+});
+
+export default VideoView;
