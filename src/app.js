60690669e74ca21631543bd9cf388f515c768a37
murog <guillermina.muro@gmail.com>
2017-12-18 11:03:48 -0800

    initialized a movieList in app.js, added movies url to movies collection

--- src/app.js
+++ src/app.js
@@ -6,8 +6,17 @@ import './css/styles.css';
 import $ from 'jquery';
 import _ from 'underscore';
 
+//import models and collections
+import MovieList from 'collections/movie_list';
+
+// import views
+import MovieListView from 'views/movie_list_view';
+
+
 // ready to go
 $(document).ready(function() {
+  const movieList = new MovieList;
+  movieList.fetch();
 
   $('#main-content').append('<p>Hello World!</p>');
 
