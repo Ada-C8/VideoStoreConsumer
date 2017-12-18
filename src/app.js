d0171d33fb6db583152bbde861a162bec447d1da
Sarah Read-Brown <srbusiness2@gmail.com>
2017-12-18 13:47:14 -0800

    minor changes to movie_list

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import './css/styles.css';
 import $ from 'jquery';
 import _ from 'underscore';
 
-// Our Project Models, Collections, Views
+// Our Components - Project Models, Collections, Views
 import LibMovie from './models/lib_movie';
 import LibMovieList from './collections/lib_movie_list';
 // template varibles
@@ -14,7 +14,7 @@ import LibMovieList from './collections/lib_movie_list';
 // ready to go
 $(document).ready(function() {
   //  underscore templates
-  
+
 
   // $('#main-content').append('<p>Hello World!</p>');
 
