855630e6866583b47bf3578d5feb06a405228952
Maria McGrew <mcgmar1@outlook.com>
2017-12-18 13:17:59 -0800

    added movie model and collection, views for both, add underscore template for movielistview

--- src/app.js
+++ src/app.js
@@ -6,9 +6,20 @@ import './css/styles.css';
 import $ from 'jquery';
 import _ from 'underscore';
 
+import Movie from './models/movie';
+import MovieList from './collections/movie_list';
+
 // ready to go
 $(document).ready(function() {
+  const movies = new MovieList();
 
+  const movieListView = new MovieListView({
+      model: movies,
+      template: _.template($('#movie-template').html()),
+      el: 'main'
+  }); // end of const
   $('#main-content').append('<p>Hello World!</p>');
+
+  movieListView.render();
 
 });
---
+++ src/views/movie_list_view.js
@@ -0,0 +1,25 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+import MovieView from '../views/movie_view';
+import Movie from '../models/movie';
+
+const MovieListView = Backbone.View.extend({
+  initialize(params) {
+    this.template = params.template;
+    this.listenTo(this.model, "update", this.render);
+  },
+  render() {
+    this.$('#movies').empty();
+    this.model.each((movie) => {
+      const movieView = new MovieView ({
+        model: movie,
+        template: this.template,
+        tagName: 'li',
+        className: 'movie'
+      });
+    }); // end of each loop
+    return this;
+  } // end of render
+}); // end MovieListView
+
+export default MovieListView;
