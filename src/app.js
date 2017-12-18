dc8c5b9ad21941b29cf7d1b7fc6140ee5e7ecc78
Kayla Ecker <kaylajayecker@gmail.com>
2017-12-18 10:31:50 -0800

    initial set up complete for imports and lists

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import './css/styles.css';
 import $ from 'jquery';
 import _ from 'underscore';
 
+// models
+import Backbone from 'backbone';
+import Customer from '/models/customer';
+import Movie from '/models/movie';
+
+//collections
+import CustomerList from '/collections/customer_list';
+import MovieList from '/collections/movie_list';
+
+//views
+import CustomerListView from '/views/customer_list_view';
+import MovieListView from '/views/movie_list_view';
+
 // ready to go
 $(document).ready(function() {
 
