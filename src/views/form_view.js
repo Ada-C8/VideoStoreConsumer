e74ac25ab1b19e2cd56dfccbef75ed2a2575e177
murog <guillermina.muro@gmail.com>
2017-12-18 14:33:14 -0800

    created form view that searches movie collection for movie based off of form input

---
+++ src/views/form_view.js
@@ -0,0 +1,58 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+
+import MovieView from '../views/movie_view';
+import Movie from '../models/movie';
+
+const FormView = Backbone.View.extend({
+  initialize(params) {
+    // this.template = params.template;
+    this.bus = params.bus;
+    this.model = params.model;
+  },
+  events: {
+    'click button.find-movie' : 'findMovie',
+    'click button.show-all' : 'showAllMovies'
+  },
+  showAllMovies: function(e) {
+    e.preventDefault();
+    if (this.$('#movies').hasClass('fetched')) {
+      this.$('#movies').toggle();
+    } else {
+      this.model.fetch();
+      this.$('#movies').addClass('fetched');
+    }
+  },
+  findMovie: function(e) {
+    e.preventDefault();
+    const title = this.$('input').val().toUpperCase();
+    let result;
+    // let movie;
+    if(this.$('#movies').hasClass('fetched')) {
+      result = this.model.findWhere({upperCaseTitle: title});
+      // debugger;
+    } else {
+      this.model.fetch();
+      this.$('#movies').addClass('fetched');
+      result = this.model.findWhere({title: title});
+
+    }
+    // debugger;
+    if (result ) {
+      this.bus.trigger(`displayMyDetails${result.get('cid')}`, result);
+
+    } else {
+      result = searchTMDB(title);
+    }
+  },
+  searchModel: function(title) {
+    const movie = this.model.findWhere({uppercasedTitle: title});
+    return movie
+  },
+  searchTMDB: function(title) {
+
+  }
+
+});
+
+export default FormView;
