65c69b6ac6ada0aee9d96bdaa1784c37e19e8684
Irene DeVera <irenneeyyy@gmail.com>
2017-12-18 13:03:20 -0800

    Finish setup first commit

---
+++ src/models/movie.js
@@ -0,0 +1,13 @@
+import Backbone from 'backbone';
+
+const Movie = Backbone.Model.extend({
+});
+
+export default Movie;
+
+
+
+
+// Search all Movies
+// Add a movie to the rental library - Add a movie, need a form
+// List all movies the rental library - Movie Collection
