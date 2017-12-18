1c7b12e1381774f6d99d91eca3ba32ff4b82902f
Amy Lee <amy.yj.lee@gmail.com>
2017-12-18 15:33:45 -0800

    now able to fetch movies from our VideoStoreAPI, need to work on rendering view for the movie list

--- src/app.js
+++ src/app.js
@@ -6,12 +6,15 @@ import './css/styles.css';
 import $ from 'jquery';
 import _ from 'underscore';
 import Movie from 'models/movie';
-import MovieList from 'collects/movie_list';
-import MovieView from 'views/movie';
-import MovieListView from 'view/movie_list';
+import MovieList from 'collections/movie_list';
+import MovieView from 'views/movie_view';
+import MovieListView from 'views/movie_list_view';
 
 // ready to go
 $(document).ready(function() {
+  const movies = new MovieList();
+  const fetchedMovies = movies.fetch();
+  console.log(fetchedMovies);
 
   $('#main-content').append('<p>Hello World!</p>');
 
--- src/collections/movie_list.js
+++ src/collections/movie_list.js
@@ -3,7 +3,7 @@ import Movie from 'models/movie';
 
 const MovieList = Backbone.Collection.extend({
   model: Movie,
-  url: 'localhost:3000/movies',
+  url: 'http://localhost:3000/movies',
   parse(response) {
     response.forEach(function(movieAttrs) {
       if (movieAttrs.inventory == null) {
