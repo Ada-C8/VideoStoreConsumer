22feb8b5130b2ef666adb603a6c32e66d8607be2
Tanisha <tanishahampden@gmail.com>
2017-12-18 15:00:30 -0800

    add render form function to main view

--- src/views/main_view.js
+++ src/views/main_view.js
@@ -6,21 +6,25 @@ import MovieListView from './movie_list_view';
 const MainView = Backbone.View.extend({
   initialize(params) {
     this.movieTemplate = params.movieTemplate;
+    this.formTemplate = params.formTemplate;
     this.movieList = params.movieList;
   },
   events: {
     'click button#our-movies' : 'renderOurMovies',
-    'click button#all-movies' : 'renderAllMovies'
+    'click button#all-movies' : 'renderForm',
+    'click button#submit' : 'renderAllMovies'
   },
   renderOurMovies() {
     const movieListView = new MovieListView({
       el: '#movies-container',
       model: this.movieList,
-      // bus: bus,
       template: this.movieTemplate,
     });
 
     movieListView.render();
+  },
+  renderForm() {
+    this.$('.form').append(this.formTemplate);
   },
   renderAllMovies() {
   },
