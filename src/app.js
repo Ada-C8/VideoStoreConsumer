67fe402f750f87456b9dc0b42c716be44ba498f1
averikitsch <aekitsch@gmail.com>
2017-12-18 11:34:36 -0800

    added templates and video list to app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
+// Import jQuery & Underscore
+import $ from 'jquery';
+import _ from 'underscore';
+import VideoList from 'collections/video_list';
+import VideoListView from 'views/video_list_view';
+
+// Import Stylesheets
 import 'css/_settings.css';
 import 'foundation-sites/dist/css/foundation.css';
 import './css/styles.css';
 
-// Import jQuery & Underscore
-import $ from 'jquery';
-import _ from 'underscore';
+
+const rentalList = new VideoList();
+
+const videoTemplate = _.template($('#video-template').html());
+const videoListTemplate = _.template($('video-list-template').html());
 
 // ready to go
 $(document).ready(function() {
+  const rentalView = new VideoListView({
+    model: rentalList,
+    template: videoListTemplate,
+    el: '#rental-library',
+  });
 
-  $('#main-content').append('<p>Hello World!</p>');
+  rentalView.render();
 
 });
--- src/collections/video_list.js
+++ src/collections/video_list.js
@@ -4,6 +4,7 @@ import Video from 'models/video';
 
 const VideoList = Backbone.Collection.extend({
   model: Video,
+  url: 'localhost:3000',
 });
 
 export default VideoList;
