8ce6c62133e61585c76c2c6e0de8ea21900e3d2a
Bianca M Fernandez <bianca.maria.fernandez.22@gmail.com>
2017-12-18 14:42:31 -0800

    Stubbing needed models, collections and views in app.js and created underscore template for movie

--- src/app.js
+++ src/app.js
@@ -9,6 +9,16 @@ import _ from 'underscore';
 // ready to go
 $(document).ready(function() {
 
-  $('#main-content').append('<p>Hello World!</p>');
+  moviesLibraryTemplate = _.template($('#movies-library-template').html());
+
+  const movies = new MovieList;////NOTE/(API call data);
+
+  const moviesLibraryView = new MoviesLibraryView({
+    el: '',
+    model: movies,
+    template: moviesLibraryTemplate,
+  });
+
+  // $('#main-content').append('<p>Hello World!</p>');
 
 });
