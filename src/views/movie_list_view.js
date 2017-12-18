a101e49e7cc8976d190b79a8eccde1a91a4eb9a9
Jan Edrozo <jedrozo@gmail.com>
2017-12-18 13:35:24 -0800

    Initial Setup: Added movie model, collection, and respective views

---
+++ src/views/movie_list_view.js
@@ -0,0 +1,10 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+import Movie from '../models/movie';
+import MovieView from '../views/movie_view';
+
+const MovieListView = Backbone.View.extend({
+
+});
+
+export default MovieListView;
