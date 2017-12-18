f301976a4496e5a922bb0a998bfd86c21355a0bc
Bennett Rahn <rahnibennett@gmail.com>
2017-12-18 15:01:35 -0800

    add search function call to apit

--- src/collections/external_movie_list.js
+++ src/collections/external_movie_list.js
@@ -2,8 +2,15 @@ import Backbone from 'backbone';
 import Movie from 'models/movie';
 
 const ExternalMovieList = Backbone.Collection.extend({
+  initialize (attributes) {
+    this.title = attributes.title;
+  },
   model: Movie,
-  urlRoot: `http://localhost:3000/movies/?query=${ this.title }`,
+  url() {
+    console.log(this.title);
+    return `http://localhost:3000/movies/?query=${ this.title }`
+  },
+
 });
 
 export default ExternalMovieList;
--- src/views/main_view.js
+++ src/views/main_view.js
@@ -2,6 +2,7 @@ import Backbone from 'backbone';
 import MovieView from './movie_view';
 import Movie from '../models/movie';
 import MovieListView from './movie_list_view';
+import ExternalMovieList from '../collections/external_movie_list';
 
 const MainView = Backbone.View.extend({
   initialize(params) {
@@ -16,13 +17,28 @@ const MainView = Backbone.View.extend({
     const movieListView = new MovieListView({
       el: '#movies-container',
       model: this.movieList,
-      // bus: bus,
       template: this.movieTemplate,
     });
 
     movieListView.model.fetch();
   },
   renderAllMovies() {
+    this.search('Jaws');
+
+  },
+
+  search(title) {
+    let externalMovieList = new ExternalMovieList({
+      title: title
+    });
+
+    const movieListView = new MovieListView({
+      el: '#movies-container',
+      model: externalMovieList,
+      template: this.movieTemplate,
+    });
+
+    movieListView.model.fetch();
   },
 });
 
