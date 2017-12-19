88146c69a72463eba4a39cf48c267d0f9fb7d31f
enigmagnetic <lauren.cardella@gmail.com>
2017-12-19 03:59:41 -0800

    Add styling. Add events and slide animation for details and add rental form in movie view. Form is not showing in the right place.

--- src/views/movie_view.js
+++ src/views/movie_view.js
@@ -5,11 +5,22 @@ const MovieView = Backbone.View.extend({
     this.template = params.template;
     this.bus = params.bus;
   },
+  events: {
+    'click button.show-details': 'slideView',
+    'click button.show-form': 'slideView',
+  },
   render() {
     const compiledTemplate = this.template(this.model.toJSON());
     this.$el.html(compiledTemplate);
     return this;
-  }
+  },
+  slideView(event) {
+    if (this.$(event.target).attr('class').includes('details')) {
+      this.$('.details').toggle({direction: 'right'}, 350);
+    } else {
+      this.$('.add-rental-form').toggle({direction: 'right'}, 350);
+    }
+  },
 })
 
 export default MovieView;
