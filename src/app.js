74a061557ed42e162f4a5ff7fb954067a72f9429
Kimberley Mackenzie <kimberleyamackenzie@gmail.com>
2017-12-18 16:31:42 -0800

    Search all movies functionality WORKING.

--- src/app.js
+++ src/app.js
@@ -8,9 +8,13 @@ import _ from 'underscore';
 
 import StoreLibrary from 'collections/store_library';
 import StoreMovie from 'models/store_movie';
+import APIMovies from 'collections/api_movies';
+import APIMovie from 'models/api_movie';
 
 import StoreLibraryView from 'views/store_library_view';
 import StoreMovieView from 'views/store_movie_view';
+import APIMoviesView from 'views/api_movies_view';
+import APIMovieView from 'views/api_movie_view';
 
 
 $(document).ready(function() {
@@ -25,7 +29,25 @@ $(document).ready(function() {
   });
   storeLibraryView.render();
 
-  
+  const apiMovies = new APIMovies();
+
+  $('.submit-btn').on('click', function(e) {
+    e.preventDefault();
+    const query = $('input[name=query]').val();
+    apiMovies.fetch({data: {query:`${query}`}});
+  });
+
+
+  const apiMoviesView = new APIMoviesView({
+    model: apiMovies,
+    template: _.template($('#api-movie-template').html()),
+    el: 'main'
+  });
+  apiMoviesView.render();
+
+
+
+
 
 
 });
