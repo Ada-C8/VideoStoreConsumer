be948a4eef671dd7e051be4e349d15fb064cfed1
Severin Wiggenhorn <severin.randall@gmail.com>
2017-12-19 11:03:04 -0800

    fixed bug where wasnt ref etching whole collect after search of collection

--- src/views/movie_list_view.js
+++ src/views/movie_list_view.js
@@ -12,81 +12,79 @@ const MovieListView = Backbone.View.extend({
     console.log(this.model);
     this.$('#list').empty();
 
-    this.model.each((movie) => {
-      const movieView = new MovieView({
-        model: movie,
-        template: this.template,
-        tagName: 'li',
-        className: 'movie',
-      });
-      this.$('#list').append(movieView.render().$el);
-  })
-  return this;
-},
-submit(event) {
-  event.preventDefault();
+    this.model.fetch({}).done(() => {
 
-  const searchType = this.$('input[name=search-type]:checked').val();
-  const searchTerm = this.$('input[name=search]').val().toUpperCase();
-  const regSearchTerm = this.$('input[name=search]').val();
-
-  if (searchType === 'search inventory') {
-    // refactor these into two seperate methods??
-    const movieList = this.model.models;
-    const filteredMovies = movieList.filter(movie => movie.get('upperCaseTitle').includes(searchTerm));
-
-    this.$('#list').empty();
-
-    filteredMovies.forEach((movie) => {
-      const movieView = new MovieView({
-        model: movie,
-        template: this.template,
-        tagName: 'li',
-        className: 'movie',
-      });
-      this.$('#list').append(movieView.render().$el);
+      this.model.each((movie) => {
+        const movieView = new MovieView({
+          model: movie,
+          template: this.template,
+          tagName: 'li',
+          className: 'movie',
+        });
+        this.$('#list').append(movieView.render().$el);
+      })
+      return this;
     });
-    return this;
-  } else {
-    console.log('Searching Imdb...');
-    // this.model.url = `http://localhost:3000/movies/?query=${regSearchTerm}`;
-    // console.log(`http://localhost:3000/movies/?query=${regSearchTerm}`);
-    console.log(this.model);
-    const imdbList = this.model.fetch({data: {query: regSearchTerm}});
-    console.log(imdbList);
-    // this.listenTo(this.model, 'change', this.render);
-    // imdbList.done((...options) => {
-    //   console.log(options);
+  },
+  submit(event) {
+    event.preventDefault();
 
-     imdbList.then((data, status, response) => {
+    const searchType = this.$('input[name=search-type]:checked').val();
+    const searchTerm = this.$('input[name=search]').val().toUpperCase();
+    const regSearchTerm = this.$('input[name=search]').val();
 
-       this.model.models.forEach((movie) => {
-         const movieView = new MovieView({
-           model: movie,
-           template: this.template,
-           tagName: 'li',
-           className: 'movie',
-         });
-         this.$('#list').append(movieView.render().$el);
-       });
-     });
-}
+    if (searchType === 'search inventory') {
+      // refactor these into two seperate methods??
+      this.model.fetch({}).done(() => {
+      const movieList = this.model.models;
+      const filteredMovies = movieList.filter(movie => movie.get('upperCaseTitle').includes(searchTerm));
 
-  //   this.$('#list').empty();
-  //
-  //   imdbList.forEach((movie) => {
-  //     const movieView = new MovieView({
-  //       model: movie,
-  //       template: this.template,
-  //       tagName: 'li',
-  //       className: 'movie',
-  //     });
-  //     this.$('#list').append(movieView.render().$el);
-  //   });
-  //   return this;
-  // }
+      this.$('#list').empty();
 
-},
+      filteredMovies.forEach((movie) => {
+        const movieView = new MovieView({
+          model: movie,
+          template: this.template,
+          tagName: 'li',
+          className: 'movie',
+        });
+        this.$('#list').append(movieView.render().$el);
+      });
+      return this;
+    });
+    } else {
+      console.log('Searching Imdb...');
+      this.$('#list').empty();
+      const imdbList = this.model.fetch({data: {query: regSearchTerm}});
+      imdbList.then((data, status, response) => {
+
+        this.model.models.forEach((movie) => {
+          const movieView = new MovieView({
+            model: movie,
+            template: this.template,
+            tagName: 'li',
+            className: 'movie',
+          });
+          this.$('#list').append(movieView.render().$el);
+        });
+      });
+    }
+
+    //   this.$('#list').empty();
+    //
+    //   imdbList.forEach((movie) => {
+    //     const movieView = new MovieView({
+    //       model: movie,
+    //       template: this.template,
+    //       tagName: 'li',
+    //       className: 'movie',
+    //     });
+    //     this.$('#list').append(movieView.render().$el);
+    //   });
+    //   return this;
+    // }
+
+  },
   events: {
     'click #movie-button': 'render',
     'click #searchButton': 'submit'
