b1ff0ee8171f988083ad9a40f75f7d4a1bd37817
Sarah Read-Brown <srbusiness2@gmail.com>
2017-12-19 11:14:23 -0800

    Able to change the url in search movie method

--- src/collections/search_movie_list.js
+++ src/collections/search_movie_list.js
@@ -2,13 +2,13 @@ import BackBone from 'backbone';
 import SearchMovie from '../models/search_movie';
 
 const SearchMovieList = Backbone.Collection.extend({
-  defaults: {
-    'searchMovieTitle': null,
-  },
-
+  // defaults: {
+  //   'searchMovieTitle': null,
+  // },
+  url: 'http://localhost:3000/movies?query=',
   model: SearchMovie,
   // urlRoot: `http://localhost:3000/movies?query=${this.get('searchTerm')}`,
-  urlRoot: `http://localhost:3000/movies?query=Jaws`
+  // urlRoot: `http://localhost:3000/movies?query=Jaws`
   // + this.get('searchMovieTitle'),
 
 });
--- src/views/search_movie_list_view.js
+++ src/views/search_movie_list_view.js
@@ -15,22 +15,19 @@ const SearchMovieListView = BackBone.View.extend({
 
   searchMovies(event){
     event.preventDefault();
+    const baseURL = '`http://localhost:3000/movies?query=';
     console.log('In searchMovies');
     console.log(this.$('#search-movie-title').val());
     const searchMovieTitle = this.$('#search-movie-title').val();
 
+    // TODO: make sure we think about reseting url in the base of multiple searches, also do we need to change user input search term if there are spaces?
     console.log(this.model);
-    console.log(this.model.urlRoot);
 
-    const fullUrl = this.model.urlRoot + searchMovieTitle
-    console.log(fullUrl);
-    console.log(this.model.searchMovieTitle);
-    console.log(this.el);
-    console.log(this.template);
-    this.model.set({searchMovieTitle: searchMovieTitle});
-    console.log(this.model.searchMovieTitle);
-    this.model.fetch();
+    this.model.url += searchMovieTitle;
 
+    this.model.fetch().then(function(response){
+      console.log(response);
+    });
   },
 
   render() {
