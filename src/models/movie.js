97a7df91f34c0a81448ea5cc93543d01fb102e11
Stef <seedsofthesesame@gmail.com>
2017-12-19 14:36:33 -0800

    Can add movies, exclamation point.

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,7 +1,11 @@
 import Backbone from 'backbone';
 
 const Movie = Backbone.Model.extend({
+  urlRoot:'http://localhost:3000/movies',
 
+  parse(response) {
+    return response
+  },
 });
 
 export default Movie;
--- src/views/movie_view.js
+++ src/views/movie_view.js
@@ -1,5 +1,8 @@
 import Backbone from 'backbone';
 
+import Movie from '../models/movie';
+
+
 const MovieView = Backbone.View.extend({
   initialize(params) {
     this.template = params.template;
@@ -9,6 +12,40 @@ const MovieView = Backbone.View.extend({
     this.$el.html(compiledTemplate);
     return this;
   },
+
+  events: {
+    'click button#btn-add-to-lib': 'addLib',
+
+  },
+
+  // addLib: function(e) {
+  //   e.preventDefault();
+  //   console.log(this.model);
+  //   const newMovie = this.model;
+  //
+  //   if (newMovie.isValid()) {
+  //     newMovie.save({}, {
+  //       success: this.successSave(newMovie),
+  //     });
+  //   }
+  // },
+
+  addLib: function(e) {
+    e.preventDefault();
+    console.log(this.model);
+    const newMovie = new Movie ({
+      title: this.model.get('title'),
+      release_date: this.model.get('release_date'),
+    })
+    newMovie.save({}, {
+      success: this.successSave(newMovie),
+    });
+  },
+
+  successSave: function(newMovie) {
+    console.log(newMovie);
+    console.log('success');
+  }
 })
 
 export default MovieView;
