e48874cb5355b51d78667b62422dd9305f64c25c
Angela Wilson <wilsonange@gmail.com>
2017-12-18 16:24:42 -0800

    render rental library to DOM

--- src/app.js
+++ src/app.js
@@ -11,23 +11,26 @@ import Movie from './models/movie';
 import MovieList from './collections/movie_list';
 
 //VIEWS
-import MoviesView from './views/movies_view';
+import MovieView from './views/movie_view';
 import MoviesLibraryView from './views/movies_library_view';
 
-const movieList = new MovieList()
 // ready to go
 $(document).ready(function() {
 
   const moviesLibraryTemplate = _.template($('#movie-template').html());
 
-  movieList.fetch()
 
-  // const movies = new MovieList;////NOTE/(API call data);
+
+
+  const movies = new MovieList();
+  movies.fetch()
 
   const moviesLibraryView = new MoviesLibraryView({
     el: '#movies-container',
     model: movies,
     template: moviesLibraryTemplate,
   });
+
+  moviesLibraryView.render();
 
 });
--- src/views/movies_library_view.js
+++ src/views/movies_library_view.js
@@ -1,11 +1,18 @@
 import Backbone from 'backbone';
 import MovieList from '../collections/movie_list';
 
-const MovieLibraryView = Backbone.View.extend({
+// views
+import MovieView from './movie_view'
+
+const MoviesLibraryView = Backbone.View.extend({
   initialize(params){
     this.template = params.template;
+    this.listenTo(this.model, 'update', this.render);
   },
   render(){
+    console.log(this);
+    console.log('this is this.model');
+console.log(this.model);
     this.model.each((movie) => {
       const movieView = new MovieView({
         model: movie,
@@ -13,10 +20,14 @@ const MovieLibraryView = Backbone.View.extend({
         className: 'movie',
         tagName: 'li',
       });
+      console.log("rendering movie view");
+      console.log('this is movie = ');
+      console.log(movie);
+      console.log(this.template);
       this.$('#movies').append(movieView.render().$el);
     });
     return this;
   },
 });
 
-export default MovieLibraryView
+export default MoviesLibraryView
