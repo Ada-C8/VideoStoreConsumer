3dc69e2ecb3167a799b39887ad887d713f49c1d4
Tamira Vojnar <tamiravojnar@gmail.com>
2017-12-19 14:34:25 -0800

    was checking if we already have the movie by making another api call, but instead we are going to change our approach to check if we have the movie locally in our MovieList collection

--- src/views/search_view.js
+++ src/views/search_view.js
@@ -1,14 +1,25 @@
 import Backbone from 'backbone';
-
+import $ from 'jquery';
 
 const SearchView = Backbone.View.extend({
   initialize(params){
     this.template = params.template
   },
   render(){
+    this.doWeHaveMovie(this.model.title)
     const compiledTemplate = this.template(this.model); //.toJSON());
     this.$el.html(compiledTemplate)
     return this
+  }, // render
+  doWeHaveMovie(movieTitle) {
+    $.get('http://localhost:3000/movies').done((data) => {
+      const allMovies = data;
+      console.log('in doWeHaveMovie');
+      console.log(allMovies);
+    })
+    console.log(`the movie title is ${movieTitle}`);
+
+
   }
 });
 
