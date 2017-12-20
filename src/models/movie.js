aab45e29d658d5f75257e1901f5b9220a2fc89ad
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-12-20 09:50:58 -0800

    validations and default values for movie model

--- src/models/movie.js
+++ src/models/movie.js
@@ -4,6 +4,21 @@ const Movie = Backbone.Model.extend({
   url: function() {
     return 'http://localhost:3000/movies/' + this.get('title');
   },
+  defaults: {
+    inventory: 0,
+    release_date: new Date(),
+    overview: "No description",
+  },
+  validate(attr) {
+    const errors = {};
+    if (!attr.title) {
+      if (errors['title']) {
+        errors['title'].push("can't be blank");
+      } else {
+        errors['title'] = ["can't be blank"];
+      }
+    }
+  },
 });
 
 export default Movie;
