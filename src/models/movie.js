69f51ebfb2ca9819ab0a5b9bbedec12a432c2715
Kimberley Zell <kimberley.zell@gmail.com>
2017-12-20 10:02:51 -0800

    moved the logic for add movie to database into the movie model

--- src/models/movie.js
+++ src/models/movie.js
@@ -3,7 +3,7 @@ import Backbone from 'backbone';
 const Movie = Backbone.Model.extend({
   urlRoot: 'http://localhost:3000/movies',
   defaults: {
-    
+
   },
   initialize(attributes) {
   },
@@ -20,6 +20,29 @@ const Movie = Backbone.Model.extend({
     // } else {
     //   return false;
     // }
+  },
+
+  add(newMovie) {
+    newMovie.save( {}, {
+      success: (model, response) => {
+        const movieSuccess = 'successfully added a movie!';
+        console.log(movieSuccess);
+        $('.display-status').html('');
+        console.log(response);
+        // $('.display-status').html(response.name + tripSuccess);
+        // $('#add-trip-form').remove();
+        // modalDisplay();
+        // reportStatus('success', 'Successfully added reservation!');
+      },
+      error: (model, response) => {
+        const movieFailure = 'Failed to save movie! Server response:';
+        // console.log(`validationError ${response.attributes['validationError']}`);
+        // $('.display-status').html('')
+        console.log(response.errors);
+        // $('.display-status').html(tripFailure);
+        // modalDisplay();
+      },
+    });
   },
 
 });
--- src/views/movie_view.js
+++ src/views/movie_view.js
@@ -29,26 +29,27 @@ const MovieView = Backbone.View.extend({
     //   $('.display-status').html(`${newTrip.validationError}`);
     //   modalDisplay();
     // }
-    newMovie.save( {}, {
-      success: (model, response) => {
-        const movieSuccess = 'successfully added a movie!';
-        console.log(movieSuccess);
-        $('.display-status').html('')
-        console.log(response);
-        // $('.display-status').html(response.name + tripSuccess);
-        // $('#add-trip-form').remove();
-        // modalDisplay();
-        // reportStatus('success', 'Successfully added reservation!');
-      },
-      error: (model, response) => {
-        const movieFailure = 'Failed to save movie! Server response:';
-        // console.log(`validationError ${response.attributes['validationError']}`);
-        // $('.display-status').html('')
-        console.log(response.errors);
-        // $('.display-status').html(tripFailure);
-        // modalDisplay();
-      },
-    });
+    model.add(newMovie);
+    // newMovie.save( {}, {
+    //   success: (model, response) => {
+    //     const movieSuccess = 'successfully added a movie!';
+    //     console.log(movieSuccess);
+    //     $('.display-status').html('')
+    //     console.log(response);
+    //     // $('.display-status').html(response.name + tripSuccess);
+    //     // $('#add-trip-form').remove();
+    //     // modalDisplay();
+    //     // reportStatus('success', 'Successfully added reservation!');
+    //   },
+    //   error: (model, response) => {
+    //     const movieFailure = 'Failed to save movie! Server response:';
+    //     // console.log(`validationError ${response.attributes['validationError']}`);
+    //     // $('.display-status').html('')
+    //     console.log(response.errors);
+    //     // $('.display-status').html(tripFailure);
+    //     // modalDisplay();
+    //   },
+    // });
   },
 });
 
