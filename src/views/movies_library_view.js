e478363fd510ad6053c3cf1ebc8952a2c913f6fe
Angela Wilson <wilsonange@gmail.com>
2017-12-19 23:00:06 -0800

    remove unused show method on movieLibraryView

--- src/views/movies_library_view.js
+++ src/views/movies_library_view.js
@@ -8,7 +8,6 @@ const MoviesLibraryView = Backbone.View.extend({
   initialize(params){
     this.template = params.template;
     this.listenTo(this.model, 'update reset', this.render);
-    this.listenTo(this.model, 'click', this.show)
   },
   render(){
     this.$('#movies').empty();
@@ -25,14 +24,6 @@ const MoviesLibraryView = Backbone.View.extend({
     });
     return this;
   },
-  events: {
-    'click button.view-library-btn': 'show'
-  },
-  show() {
-    console.log('I am in show()');
-    this.model.trigger('click')
-    this.$('#movies-container').show()
-  }
 });
 
 export default MoviesLibraryView
