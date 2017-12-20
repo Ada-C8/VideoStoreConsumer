1149936624d59eb5b6b277df72970f3f4a870a79
Tamira Vojnar <tamiravojnar@gmail.com>
2017-12-19 16:25:27 -0800

    only adds the 'add to library' button if you don't already have the movie

--- src/views/search_view.js
+++ src/views/search_view.js
@@ -3,24 +3,37 @@ import $ from 'jquery';
 
 const SearchView = Backbone.View.extend({
   initialize(params){
-    this.template = params.template
+    this.template = params.template;
+    this.bus = params.bus;
+
+    this.listenTo(this.bus,`${this.model.title}${this.model.release_date}`, this.setHave )
   },
   render(){
-    this.doWeHaveMovie(this.model.title)
+    console.log('in render for search view');
+    const movieData = {
+      title: this.model.title,
+      release_date: this.model.release_date}
+    this.doWeHaveMovie(movieData)
     const compiledTemplate = this.template(this.model); //.toJSON());
     this.$el.html(compiledTemplate)
     return this
   }, // render
-  doWeHaveMovie(movieTitle) {
-    $.get('http://localhost:3000/movies').done((data) => {
-      const allMovies = data;
-      console.log('in doWeHaveMovie');
-      console.log(allMovies);
-    })
-    console.log(`the movie title is ${movieTitle}`);
+  doWeHaveMovie(movieData) {
+    console.log('in doWeHaveMovie');
+    console.log(movieData);
+    this.bus.trigger('lookForMovie', movieData)
+  }, // doWeHaveMovie
+  setHave(haveIt) {
+    console.log('in have it');
+    if (haveIt) {
+      console.log('in if');
+      console.log(this.model);
+      this.model['have'] = true;
+    } else {
+      this.model['have'] = false;
+    } // if
+  } // setHave
 
-
-  }
 });
 
 export default SearchView
