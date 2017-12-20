2535d4b3fed2a5afcee457190c2b75d811e32cf4
VirtualLindsey <tapircode@gmail.com>
2017-12-20 01:28:56 -0800

    validations:

--- src/models/movie.js
+++ src/models/movie.js
@@ -2,7 +2,12 @@ import Backbone from 'backbone';
 
 const Movie = Backbone.Model.extend({
   initialize(attributes){
-    url: 'http://localhost:3000/movies'
+    this.title = attributes.title,
+    this.overview = attributes.overview,
+    this.release_date = attributes.release_date,
+    this.image_url = attributes.image_url,
+    this.bus = attributes.bus,
+    this.url = 'http://localhost:3000/movies'
   },
   parse: function(response){
     let movie = {
@@ -10,6 +15,25 @@ const Movie = Backbone.Model.extend({
       overview: response['overview']
     }
     return movie
+  },
+  validate(attributes){
+    const errors = {}
+
+    if (!attributes.title){
+      errors['title'] = 'The title can not be blank';
+    }
+
+    if (!attributes.overview){
+      errors['title'] = 'The overview can not be blank';
+    }
+
+    if (!attributes.release_date){
+      errors['title'] = 'The release date can not be blank';
+    }
+
+    if (!attributes.image_url){
+      errors['title'] = 'The image url can not be blank';
+    }
   },
 });
 
